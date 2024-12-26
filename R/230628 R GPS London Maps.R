9a272edf86d4a3b103c54eeff66ca458061025cd
DAChristina <d.christina23@imperial.ac.uk>
2024-12-26 20:13:25 +0700

    ENJOY!

--- R/230628 R GPS London Maps.R
+++ R/230628 R GPS London Maps.R
@@ -1,174 +1,73 @@
 # 2023 Geo spatial data for London Maps (Including Expensive zones, ehe~)
 library(tidyverse)
-library(rgdal)
-library(sf)
-library(readxl)
-library(leaflet)
-################################################################################
-# 1. INTRO: Load the Geo spatial file
-# TRIAL to use a shapefile (*.shp) files first!
-################################################################################
 
-# 1. Unzip file coz' file downloaded from GADM is in *.zip
-# UK_shp_zip = '/home/ron/Downloads/2023 LPDP GUIDES & Documents/2023 London Maps/gadm41_GBR_shp.zip'
-# UK_shp_out = '/home/ron/Downloads/2023 LPDP GUIDES & Documents/2023 London Maps/gadm41_GBR_shp'
-# unzip (UK_shp_zip, exdir=UK_shp_out)
-
-# 2. Load
+# DO NOT FILTER SF DATA BY USING dplyr!
 UK_spdf <- sf::read_sf(dsn = "raw_data/gadm41_GBR_shp/gadm41_GBR_4.shp")
+UK_spdf_sf <- sf::st_as_sf(UK_spdf, coords = c("longitude", "latitude"), crs = '4326')
 
-# summary(UK_spdf) # tells you the max and min coordinates, the kind of projection in use
-# length(UK_spdf) # how many regions you have
-
-head(UK_spdf@data)
-# glimpse(UK_spdf)
-
-# Compare NAME 1 vs. NAME 2, just being curious to find LONDON
-unique(UK_spdf$NAME_1) # Name 1 = 4 constituent countries
-unique(UK_spdf$NAME_2) # Name 2 = seems like regencies & cities
-
-unique(UK_spdf[grepl('London', UK_spdf$NAME_2), ]$NAME_2)
-# London is here (NAME_2) under the name of "Greater London"
-
-# Filter GPS data to Greater London
-G_LDN <- UK_spdf[UK_spdf$NAME_2 %in% c('Greater London'), ]
+G_LDN <- UK_spdf_sf[UK_spdf_sf$NAME_2 %in% c('Greater London'), ]
+G_LDN_sf <- sf::st_as_sf(G_LDN, coords = c("longitude", "latitude"), crs = '4326')
 
 # Casually create a map by using ggplot2, ehe~
-ggplot() +
-  geom_polygon(data = G_LDN,
-               aes( x = long, y = lat, group = group),
-               fill="#69b3a2", color="white") +
-  theme_void()
-
-# NEXT:
-# U have to combine *.csv OR *.xlsx data to *.shp (CANNOT BE RUN VICE-VERSA)!!!
-# Convert the 'SpatialPolygonsDataFrame' to 'sf' object first!
-
-# Combine the df to *.shp data (CANNOT BE RUN VICE-VERSA)!!!
-# Convert the 'SpatialPolygonsDataFrame' to 'sf' object first:
-# Recall GBR_spdf <- st_read(dsn = GBR_shp_path_LINUX)
-
-# Notes: DO NOT Filter sf Data!!!!
-UK_spdf_sf <- sf::st_as_sf(UK_spdf, coords = c("longitude", "latitude"), crs = '4326')
-glimpse(GBR_spdf_sf)
+# ggplot(data = G_LDN_sf ) +
+#   geom_sf() +
+#   xlab("Longitude") + ylab("Latitude") +
+#   theme_bw()
 
 
-
-################################################################################
-# 2. Load & Data wrangling for Greater London areas,
-# Specifically I want to highlight the expensive zones
-
-# Workflow:
-# 1. Create/load the df
-################################################################################
-
-# First of all, I didn't know that the gov have the data that I needed O.o
-# SOURCE: https://data.london.gov.uk/dataset/london-workplace-zone-classification
 # London Workplace Zone Classification (LWZC)
-
-G_LDN_WZ_Path_LINUX = 'raw_data/LWZC Classification.xls'
-G_LDN_WZ <- read_excel(G_LDN_WZ_Path_LINUX)
-head(G_LDN_WZ)
-unique(G_LDN_WZ$'LA name')
-unique(G_LDN$NAME_3) # Name 3 = seems like boroughs/administrative divisions/districts
-names(G_LDN_WZ) <- gsub(" ", "_", names(G_LDN_WZ))
-head(G_LDN_WZ)
+# SOURCE: https://data.london.gov.uk/dataset/london-workplace-zone-classification
+G_LDN_WZ <- readxl::read_excel("raw_data/LWZC Classification.xls") %>% 
+  dplyr::rename_all(~stringr::str_replace_all(., " ", "_")) %>% 
+  dplyr::mutate(LA_name=str_replace(LA_name, "Westminster", "City of Westminster"),
+                Expensiveness = case_when(
+                  SubGroup == "FF" ~ 0,
+                  SubGroup == "A1" ~ 8,
+                  SubGroup == "A2" ~ 11,
+                  SubGroup == "B1" ~ 1,
+                  SubGroup == "B2" ~ 2,
+                  SubGroup == "C1" ~ 9,
+                  SubGroup == "C2" ~ 10,
+                  SubGroup == "D1" ~ 6,
+                  SubGroup == "D2" ~ 7,
+                  SubGroup == "D3" ~ 5,
+                  SubGroup == "E1" ~ 4,
+                  SubGroup == "E2" ~ 3
+                )
+  ) %>% 
+  dplyr::group_by(LA_name, SubGroup, Subgroup_description, Expensiveness) %>%
+  dplyr::summarise(countt = n()) %>%
+  dplyr::ungroup()
 
 # Check name consistency in GADM database AND gov data:
-NeedToBeCorrected_gov <- G_LDN_WZ[!G_LDN_WZ$LA_name %in% G_LDN$NAME_3,]
-unique(NeedToBeCorrected_gov$LA_name)
-
-NeedToBeCorrected_GADM <- G_LDN[!G_LDN$NAME_3 %in% G_LDN_WZ$LA_name,]
-unique(NeedToBeCorrected_GADM$NAME_3)
-# 'Westminster' --> 'City of Westminster'
-# But we'll keep it for later since modifying *.shp files need library(sp)
-# Now, we only change the gov data so both files can be merged!
-
-# FILTER & data wrangling the gov file:
-G_LDN_WZ <- G_LDN_WZ %>% 
-  # filter('LA name' != '???') %>% # We'll do the filter if necesary ;)
-  mutate(LA_name=str_replace(LA_name, "Westminster", "City of Westminster"))
-
-unique(G_LDN_WZ$LA_name)
-# For now, the data is "City of Westminster", but we will change it back later ;)
-
-# Find Group & SubGroup classification,
-# I'll classify it by myself based on categories of Groups & Subgroups
-
-head(G_LDN_WZ)
-
-Group_desc <- G_LDN_WZ %>%
-  group_by(Group, Group_description, SubGroup, Subgroup_description) %>%
-  summarise(countt = n()) %>%
-  view()
-
-# Only 12 categories found!
-# How about we create new variable called 'Expensiveness' (5 point Likert scale)
-# 0 = Unclassified, 5 = very expensive
-G_LDN_WZ <- G_LDN_WZ %>%
-  mutate(Expensiveness = case_when(
-    SubGroup == "FF" ~ 0,
-    SubGroup == "A1" ~ 3,
-    SubGroup == "A2" ~ 1,
-    SubGroup == "B1" ~ 5,
-    SubGroup == "B2" ~ 5,
-    SubGroup == "C1" ~ 2,
-    SubGroup == "C2" ~ 2,
-    SubGroup == "D1" ~ 4,
-    SubGroup == "D2" ~ 3,
-    SubGroup == "D3" ~ 4,
-    SubGroup == "E1" ~ 5,
-    SubGroup == "E2" ~ 5
-  )) %>%
-  view()
-
-
-head(G_LDN_WZ)
-unique(G_LDN_WZ$Group_description)
-unique(G_LDN_WZ$Subgroup_description)
-
-# Hmm, since each boroughs contains many variations of Group,
-# How about we group the df by LA_name, and state the group by the dominant one? 
-grouped_G_LDN_WZ <- G_LDN_WZ %>%
-  group_by(LA_name, Subgroup_description, Expensiveness) %>%
-  summarise(countt = n()) %>%
-  ungroup() %>%
-  view()
+# NeedToBeCorrected_gov <- G_LDN_WZ[!G_LDN_WZ$LA_name %in% G_LDN$NAME_3,]
+# unique(NeedToBeCorrected_gov$LA_name)
+# NeedToBeCorrected_GADM <- G_LDN[!G_LDN$NAME_3 %in% G_LDN_WZ$LA_name,]
+# unique(NeedToBeCorrected_GADM$NAME_3)
 
 # Find the most dominant subgroup for each boroughs
-Expensiveness_G_LDN <- grouped_G_LDN_WZ %>%
+Expensiveness_G_LDN <- G_LDN_WZ %>%
   group_by(LA_name) %>%
   filter(countt == max(countt)) %>%
   #top_n(n = 3, wt = countt) %>%
-  ungroup() %>%
-  view()
+  ungroup()
 
-# 3. Combine Expensiveness_G_LDN to *.shp data (CANNOT BE RUN VICE-VERSA)!!!
-# Convert the 'SpatialPolygonsDataFrame' to 'sf' object first:
-G_LDN_sf <- st_as_sf(G_LDN, coords = c("longitude", "latitude"), crs = '4326')
-# view(G_LDN_sf)
-glimpse(G_LDN_sf)
+# London Local Authority name
+# SOURCE: https://geoportal.statistics.gov.uk/datasets/42af123c4663466496dafb4c8fcb0c82_0/explore
+# LA_names <- read.csv("raw_data/Local_Authority_Districts_(December_2022)_Names_and_Codes_in_the_United_Kingdom.csv")
+
 
 Comm_merged <- merge(G_LDN_sf, Expensiveness_G_LDN, by.x = 'NAME_3', by.y = 'LA_name', all.x = TRUE) %>% 
   #replace_na(list(Expensiveness = 'Not Targeted')) %>% 
   #filter(Expensiveness != 'Not Targeted') %>% 
   mutate(Expensiveness = as.numeric(Expensiveness))
 
-view(Comm_merged)
-glimpse(Comm_merged)
-
-################################################################################
-# 3.1. LOAD interactive maps (LEAFLET), map1
-################################################################################
-# 3.1. for hovered label!
-
-
-
-# Leaflet TRIAL!
-mypalette <- colorNumeric(palette='Reds',
+# Leaflet
+mypalette <- leaflet::colorNumeric(palette='Reds',
                           domain=c(Comm_merged$Expensiveness),
-                          na.color="transparent")
-mypalette(c(0,10))
+                          na.color="transparent",
+                          reverse = TRUE)
+mypalette(c(0,120))
 
 mytext1 <- paste(
   'District: ', Comm_merged$NAME_3,'<br/>', 
@@ -176,19 +75,21 @@ mytext1 <- paste(
   sep="") %>%
   lapply(htmltools::HTML)
 
-map1 <- leaflet(Comm_merged) %>% 
-  addTiles()  %>% 
-  setView( lat=51.5033, lng=-0.1195, zoom=9) %>%
-  addPolygons( stroke = F, fillOpacity = 0.5,
-               fillColor = ~mypalette(Expensiveness),
-               label = mytext1,
-               labelOptions = labelOptions( 
-                 style = list('font-weight' = 'normal', padding = '3px 8px'), 
-                 textsize = '15px', 
-                 direction = 'auto')) %>% 
-  addLegend('bottomright', pal = mypalette, values = ~Expensiveness,
+map1 <- leaflet::leaflet(Comm_merged) %>% 
+  leaflet::addTiles()  %>% 
+  leaflet::setView( lat=51.5033, lng=-0.1195, zoom=10) %>%
+  leaflet::addPolygons(stroke = T, color = "white", weight = 0.5,
+                       fillOpacity = 0.5,
+                       fillColor = ~mypalette(Expensiveness),
+                       label = mytext1,
+                       labelOptions = leaflet::labelOptions(
+                         style = list('font-weight' = 'normal', padding = '3px 8px'),
+                         direction = 'auto')) %>% 
+  leaflet::addLegend('bottomright', pal = mypalette, values = ~Expensiveness,
             title = 'Expensive',
-            # labFormat = labelFormat(prefix = "$"),
             opacity = 1)
 
 map1
+
+htmlwidgets::saveWidget(map1, file = "outputs/interactive_map_expensiveness.html",
+                        selfcontained = TRUE)
